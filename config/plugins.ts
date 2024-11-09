4b316b51ad1a24574ef6a12952608b0f02729568
IAmDolphin1998 <giuseppe.defina1998@libero.it>
2024-11-09 17:12:42 +0100

    Initial Strapi + Document, Author, Area & Tags Content Types

---
+++ config/plugins.ts
@@ -0,0 +1,38 @@
+export default ({ env }) => ({
+  "strapi-algolia": {
+    enabled: true,
+    config: {
+      apiKey: env("ALGOLIA_ADMIN_KEY"),
+      applicationId: env("ALGOLIA_APP_ID"),
+      contentTypes: [
+        {
+          name: "api::document.document",
+          index: "document_index",
+          populate: {
+            Authors: {
+              fields: ["FirstName", "LastName", "Email"],
+              populate: {
+                Avatar: {
+                  fields: ["url", "alternativeText", "width", "height"],
+                },
+              },
+            },
+            Area: {
+              fields: ["Name"],
+            },
+            Tags: {
+              fields: ["Name"],
+            },
+            Thumbnail: {
+              fields: ["url", "alternativeText", "width", "height"],
+            },
+          },
+          hideFields: ["createdAt", "updatedAt", "publishedAt", "updatedBy"],
+        },
+      ],
+    },
+  },
+  "generate-data": {
+    enabled: true,
+  },
+});
