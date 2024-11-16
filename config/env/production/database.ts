e55128a380acc954664d0bb13c32ff0e7eccc9a6
IAmDolphin1998 <giuseppe.defina1998@libero.it>
2024-11-16 14:13:35 +0100

    Add Configs & GCS Provider (Production Environment)

---
+++ config/env/production/database.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export default ({ env }) => {
+  return {
+    connection: {
+      client: 'postgres',
+      postgres: {
+        connection: {
+          connectionString: env('DATABASE_URL', ''),
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          ssl: env.bool('DATABASE_SSL', false) && {
+            key: env('DATABASE_SSL_KEY', undefined),
+            cert: env('DATABASE_SSL_CERT', undefined),
+            ca: fs.readFileSync(env('DATABASE_CA_PATH', path.resolve(__dirname, '../../certs/postgresql-cert.pem'))).toString(),
+            capath: env('DATABASE_SSL_CAPATH', undefined),
+            cipher: env('DATABASE_SSL_CIPHER', undefined),
+            rejectUnauthorized: env.bool(
+              'DATABASE_SSL_REJECT_UNAUTHORIZED',
+              true
+            ),
+          },
+          schema: env('DATABASE_SCHEMA', 'public'),
+        },
+        pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      },
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+    },
+  };
+};
