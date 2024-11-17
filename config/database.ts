4896f66cba2b439ef6e47494dd4ac05b41b7420a
IAmDolphin1998 <giuseppe.defina1998@libero.it>
2024-11-17 13:05:46 +0100

    Update database settings

--- config/database.ts
+++ config/database.ts
@@ -4,16 +4,13 @@ export default ({ env }) => {
   return {
     connection: {
       client: 'sqlite',
-      sqlite: {
-        connection: {
-          filename: path.join(
-            __dirname,
-            '..',
-            '..',
-            env('DATABASE_FILENAME', '.tmp/data.db')
-          ),
-        },
-        useNullAsDefault: true,
+      connection: {
+        filename: path.join(
+          __dirname,
+          '..',
+          '..',
+          env('DATABASE_FILENAME', '.tmp/data.db')
+        ),
       },
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
